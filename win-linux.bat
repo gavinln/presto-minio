88ea7112b10dca10a092463d721757c18fb6fb85
Gavin Noronha <gavinln@hotmail.com>
2020-02-15 13:45:19 -0800

    updated

---
+++ win-linux.bat
@@ -0,0 +1,5 @@
+@echo off
+set ROOT_DIR=%~dp0
+echo starting Windows subsystem for Linux in %ROOT_DIR%
+REM ubuntu.exe run "cd $(wslpath -u '%ROOT_DIR%'); bash"
+wsl -e /bin/bash -l -c "cd $(wslpath '%ROOT_DIR%');/bin/bash"
