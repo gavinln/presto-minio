c975e1f14f55e13c2425f3c7e1290c35682c3fa9
Gavin Noronha <gavinln@hotmail.com>
2020-03-03 22:04:09 -0800

    add example of parsing Presto queries

--- python/presto-api.py
+++ python/presto-api.py
@@ -1,9 +1,15 @@
 import logging
 from pathlib import Path
+import json
 import sqlite3
 
+from datetime import datetime
 from typing import List
 from typing import Dict
+
+from urllib .parse import urlparse, urlunparse
+
+from pydantic import BaseModel, parse_obj_as
 
 import requests
 
@@ -15,28 +21,61 @@ SCRIPT_DIR = Path(__file__).parent.resolve()
 log = logging.getLogger(__name__)
 
 
-def query_list():
-    presto_url = 'http://10.0.0.2:8080/v1/query'
+def retrieve_query_list(netloc=None):
+    if netloc is None:
+        netloc = '10.0.0.2:8080'
+
+    scheme = 'http'
+    path = '/v1/query'
+
+    presto_url = urlunparse((scheme, netloc, path, '', '', ''))
     response = requests.get(presto_url)
     response.raise_for_status()
     return response.json()
 
 
-class QueryList(list):
+def retrieve_query(query_url):
+    response = requests.get(query_url)
+    response.raise_for_status()
+    return response.json()
 
+
+def get_query_url(query_obj, netloc):
+    uo = urlparse(query_obj.self)
+    query_url = urlunparse(
+        (uo.scheme, netloc, uo.path, '', '', '')
+    )
+    return query_url
+
+
+class QuerySummary(BaseModel):
+    queryId: str
+    state: str
+    self: str
+    errorType: str = None
+
+
+class QueryList(list):
     @classmethod
     def retrieve(cls):
-        ql = QueryList()
-        for query in query_list():
-            ql.append(query)
-        return ql
+        netloc = '10.0.0.2:8080'
+        ql = retrieve_query_list(netloc)
+
+        qs_list = parse_obj_as(List[QuerySummary], ql)
+        qs = qs_list[2]
+        query_url = get_query_url(qs, netloc)
+        query = retrieve_query(query_url)
+        print(json.dumps(query['inputs']))
+        return QueryList(qs_list)
+
+    def to_frame(self):
+        return pd.DataFrame.from_records(qs.dict() for qs in self)
 
 
 def main():
-    print('in main')
     ql = QueryList.retrieve()
-    print(ql)
-    # embed()
+    df = ql.to_frame()
+    print(df)
 
 
 if __name__ == '__main__':
