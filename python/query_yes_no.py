7a4fa301ab637e81e7cb1b855870d0ad9fb9f6ef
Gavin Noronha <gavinln@hotmail.com>
2020-08-31 07:32:05 -0700

    add lib to query user

---
+++ python/query_yes_no.py
@@ -0,0 +1,52 @@
+import sys
+import logging
+import pathlib
+
+
+logging.basicConfig(level=logging.WARNING)
+log = logging.getLogger(__name__)
+
+
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    # https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+
+def main():
+    print('in main')
+
+
+if __name__ == "__main__":
+    main()
